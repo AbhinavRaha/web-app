3b636cf8dbdb861faa26f4a91f176634f9e01b9d
AbhinavRaha <89091790+AbhinavRaha@users.noreply.github.com>
2022-06-02 08:37:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function take_snapshort()
+{
+console.loge(img_id);
+
+webcam_snap(function(data_url){
+if(img_id=="selfie1"){
+document.getElementById("result1").innerHTML='<imgid="selfie1"src"
+}
+}
+}
